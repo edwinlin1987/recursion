54311254b0f365c4b0610c0d9f202a9d1d21a4f9
Edwin <edwinlin1987@gmail.com>
2015-03-29 09:01:35 -0700

    getElementsByClassName - implemented check function to determine if a particular element has a class, implemented recursive checkChild function to add an element to the list if it has a class and to repeat the same task for all children until each child has no more children. Passes all tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,34 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className) {
   // your code here
+  var list = [];
+
+  var check = function(element, name) {
+  	var i;
+
+  	if (element.classList === undefined) { return false }
+
+  	for (i = 0; i < element.classList.length; i++) {
+  		if (element.classList[i] === name) { return true }
+  	}
+  	return false;
+  };
+
+  var checkChild = function(element, name) {
+  	var j;
+  	if (check(element, name)) { list.push(element) }
+
+  	if (element.childNodes === []) { return }
+
+  	for (j = 0; j < element.childNodes.length; j++) {
+  		checkChild(element.childNodes[j], name);
+  	}
+  	return;
+  };
+
+  checkChild(document.body, className);
+
+  return list;
 };
