33a683adc43ace1bfb631c9907e8c4028112945a
Edwin <edwinlin1987@gmail.com>
2015-04-02 17:47:48 -0700

    parseJSON - completed all functions - tests still aren't passing, may need to add more checks in

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -8,33 +8,49 @@ var parseJSON = function(json) {
   var capture;
 
 
-  function nextChar(item) {
-  	if (item != capture) {
-  		throw new Exception(item + 'does not match' + capture);
-  	}
+  var nextChar = function(item) {
+
+  	if (item != capture) { throw new Exception(item + 'does not match' + capture); }
   	capture = json[index];
   	index++;
   	return capture;
+
+  };
+
+  var Exception = function(m) {
+  	this.m = m;
   }
 
 
 
-  //Removes whitespace from either end of a segment of text
-  var skip = function(text) {
-  }
+
+  //Skips whitespace
+  var skip = function() {
+
+  	while (capture == ' ') { nextChar(); }
+
+  };
   
 
 
   //Function for running through parse tests
 
-  var parse = function(text) {
+  var parse = function() {
 
-  }
+  	skip();
+  	if (capture == '{') { return object(); }
+  	if (capture == '[') { return array(); }
+  	if (capture == '"') { return string(); }
+  	if (/\d[-]/.test(capture)) { return number(); }
+  	return phrase();
+
+  };
 
 
 
   // Function for parsing booleans and null values
   var boolNull = function() {
+
   	if (capture == 't') { 
   		nextChar('t');
   		nextChar('r');
@@ -56,53 +72,97 @@ var parseJSON = function(json) {
   		return null; 
   	}
 	  throw new Exception('Was expecting true, false, or null');
-	}
+	};
 
 	// Function for parsing numbers
   var num = function() {
-  	var number = ''; 
-  	if (capture == '-') { number += next() }
-  	while ((/\d/).test(capture)) { number += next() }
-  	if (capture == '.') { number += next() }
-  	while ((/\d/).test(capture)) { number += next() }
+
+  	var number = '';
+
+  	if (capture == '-') { number += nextChar() }
+  	while ((/\d/).test(capture)) { number += nextChar() }
+  	if (capture == '.') { number += nextChar() }
+  	while ((/\d/).test(capture)) { number += nextChar() }
   	return number = +number;
-  }
+  };
 
   // Function for parsing strings - INCOMPLETE
   var phrase = function() {
+
   	var string = '';
-  	if (capture == '"') {
-  		next();
-  		while (capture != '"') {
-  			if (capture == '\\') {
-  				next();
-  				if (capture == '\\') { string += '\\'; }
-  				if (capture == '"') { string += '\"'; }
-  				if (capture == 'n') { string += '\n'; }
-  				if (capture == 'r') { string += '\r'; }
-  				if (capture == 'v') { string += '\v'; }
-  				if (capture == 't') { string += '\t'; }
-  				if (capture == 'b') { string += '\b'; }
-  				if (capture == 'f') { string += '\f'; }
-  			}
-  			string += next();
-  		}
-  		next();
-  		return string;
-  	}
-  	throw new Exception('Was expecting a string') 
-  	
-	}
 
-  // Function for parsing arrays - INCOMPLETE
-  var array = function(text) {
+		nextChar();
+		while (capture != '"') {
+			if (capture == '\\') {
+				nextChar();
+				if (capture == '\\') { string += '\\'; }
+				if (capture == '"') { string += '\"'; }
+				if (capture == 'n') { string += '\n'; }
+				if (capture == 'r') { string += '\r'; }
+				if (capture == 'v') { string += '\v'; }
+				if (capture == 't') { string += '\t'; }
+				if (capture == 'b') { string += '\b'; }
+				if (capture == 'f') { string += '\f'; }
+			}
+			string += nextChar();
+		}
+		nextChar();
+		return string;
+  	 
+  	
+	};
+
+  // Function for parsing arrays
+  var array = function() {
+
   	var list = [];
-  	if
-  	
-  }
-
+		nextChar();
+		skip();
+		if (capture == ']') {
+			nextChar();
+			return [];
+		}
+		while (capture != ']') {
+			list.push(parse());
+			skip();
+			nextChar();
+			if (capture == ',') {
+				nextChar();
+				white();
+			}
+			if (capture == undefined) {
+				throw new Exception('Expected an array');
+			}
+		}
+		return list;
+  };
+ 
   var obj = function(text) {
+
+  	var key;
+  	var object = {};
+
+  	nextChar();
+  	skip();
+  	if (capture == '}') {
+  		return {};
+  	}
+  	while (capture != '}') {
+  		key = parse();
+  		if (typeof(key) != 'string') {
+  			throw new Exception('Expected a string')
+  		}
+  		skip();
+  		nextChar(':');
+  		object[key] = parse();
+  		skip();
+  		if (capture == ',') {
+  			nextChar();
+  			white();
+  		}
+  	}
+  	return object;
   	
-  }
-  return parse(json);
+  };
+  return parse();
 }; 
