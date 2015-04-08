c9467bd873eaffae0d68369af737c94035cb8b7e
Edwin <edwinlin1987@gmail.com>
2015-04-07 17:39:01 -0700

    parseJSON - added nextChar step function and skip whitespaces function

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,6 +3,25 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here 
+  // your code goes here
+  var index;     
+  var cap;     
+
+  // Step through function
+  var nextChar = function (item) {
+
+    if (item && item != cap) { throw undefined }
+    cap = text[index];
+    index++;
+    return cap;
+
+  };
+
+  // Skip whitespaces function
+  var skip = function () {
+
+    while (cap == ' ') { nextChar() }
+
+  };
 
 };
